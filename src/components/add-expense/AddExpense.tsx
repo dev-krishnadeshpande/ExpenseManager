111cc55b61914cf88db62d0abf9b29162fb2f4fb
dev-krishnadeshpande <krishnadeshpande.dev@gmail.com>
2024-02-25 10:05:42 +0530

    add expense form

--- src/components/add-expense/AddExpense.tsx
+++ src/components/add-expense/AddExpense.tsx
@@ -1,7 +1,91 @@
-import React from "react";
+import {
+  TextField,
+  PrimaryButton,
+  Dropdown,
+  IDropdownOption,
+} from "@fluentui/react";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface AddExpenseForm {
+  expenseType?: string;
+  expenseDate: string;
+  expenseAmount: string;
+  description: string;
+}
 
 const AddExpense = () => {
-  return <div>AddExpense</div>;
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    reset,
+    formState: { errors },
+  } = useForm<AddExpenseForm>();
+
+  const onSubmit: SubmitHandler<AddExpenseForm> = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  const handleDropdownChange = (
+    event: React.FormEvent<HTMLDivElement>,
+    option?: IDropdownOption
+  ) => {
+    if (option) {
+      setValue("expenseType", option.text);
+    }
+  };
+
+  const options: IDropdownOption[] = [
+    { key: "cash", text: "Cash" },
+    { key: "card", text: "Card" },
+  ];
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{
+        width: "40vw",
+        margin: "auto",
+        marginTop: "70px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "24px",
+      }}
+    >
+      <Dropdown
+        placeholder="Select Expense Type"
+        label="Expense Type"
+        options={options}
+        onChange={handleDropdownChange}
+        defaultValue=""
+        required
+      />
+      <TextField
+        label="Expense Date"
+        placeholder="dd/mm/yyyy"
+        {...register("expenseDate")}
+        required
+      />
+      <TextField
+        label="Expense Amount"
+        placeholder="Enter Amount (in INR)"
+        required
+        {...register("expenseAmount")}
+      />
+      <TextField
+        label="Description"
+        placeholder="Enter Description"
+        multiline
+        rows={4}
+        {...register("description")}
+      />
+      <PrimaryButton type="submit" style={{ width: "25%" }}>
+        Add Expense
+      </PrimaryButton>
+    </form>
+  );
 };
 
 export default AddExpense;
